9f40939f7cac8f8cf72380b0d78ce68cb0446f3a
Dag König <dagk@microsoft.com>
2018-07-11 14:57:36 +0200

    creating vertex

--- Program.cs
+++ Program.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using CommandLine;
 using Gremlin.Net.Driver;
 using Gremlin.Net.Structure.IO.GraphSON;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Gremlin.Net.Structure;
+using Gremlin.Net.Driver.Remote;
 
 /* 
     Documentation
     
+    Connection String
+        Example: 
+            AccountEndpoint=https://<cosmosdb-name>.gremlin.cosmosdb.azure.com:443/;AccountKey=yh[...]==;ApiKind=Gremlin;Database=db01;Collection=col01
+        Run in Code:
+            Add -c <connection string> in the args collection in launch.json
+            
     Third party components
         * Chance - https://github.com/gmantaos/Chance.NET
         * CommandLineParser - https://github.com/commandlineparser/commandline
@@ -27,34 +35,35 @@ namespace cosmosdb_graph_test
 
         private static string unparsed_connection_string;
         private static string accountEndpoint = "";
+        private static int port = 443;
         private static string accountKey = "";
         private static string apiKind = "";
         private static string database = "";
         private static string collection = "";
+        private static GremlinClient gremlinClient;
 
-
-        private static Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
-        {
-            { "Cleanup",        "g.V().drop()" },
-            { "AddVertex 1",    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)" },
-            { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)" },
-            { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')" },
-            { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')" },
-            { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
-            { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
-            { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
-            { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
-            { "CountVertices",  "g.V().count()" },
-            { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
-            { "Project",        "g.V().hasLabel('person').values('firstName')" },
-            { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
-            { "Traverse",       "g.V('thomas').out('knows').hasLabel('person')" },
-            { "Traverse 2x",    "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
-            { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
-            { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
-            { "CountEdges",     "g.E().count()" },
-            { "DropVertex",     "g.V('thomas').drop()" },
-        };
+        // private static Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
+        // {
+        //     { "Cleanup",        "g.V().drop()" },
+        //     { "AddVertex 1",    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)" },
+        //     { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)" },
+        //     { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')" },
+        //     { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')" },
+        //     { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
+        //     { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
+        //     { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
+        //     { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
+        //     { "CountVertices",  "g.V().count()" },
+        //     { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
+        //     { "Project",        "g.V().hasLabel('person').values('firstName')" },
+        //     { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
+        //     { "Traverse",       "g.V('thomas').out('knows').hasLabel('person')" },
+        //     { "Traverse 2x",    "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
+        //     { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
+        //     { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
+        //     { "CountEdges",     "g.E().count()" },
+        //     { "DropVertex",     "g.V('thomas').drop()" },
+        // };
 
         static async Task Main(string[] args)
         {
@@ -63,59 +72,40 @@ namespace cosmosdb_graph_test
 
             unparsed_connection_string = ((Parsed<CommandLineOptions>)result).Value.ConnectionString;
             ParseUnparsedConnectionString(unparsed_connection_string);
-            if(DoWeHaveAllParameters())
+            if (DoWeHaveAllParameters())
             {
                 // Let's start
+                var gremlinServer = new GremlinServer(accountEndpoint, port, enableSsl: true,
+                                                        username: "/dbs/" + database + "/colls/" + collection,
+                                                        password: accountKey);
+
+
+                // this doesn't work in CosmosDB
+                // var graph = new Graph();
+                // var g = graph.Traversal().WithRemote(new DriverRemoteConnection(new GremlinClient(gremlinServer)));
+                // var vertex = g.AddV("person").Property("name", "kalle");
+
+                gremlinClient = new GremlinClient(gremlinServer, new GraphSON2Reader(), new GraphSON2Writer(), GremlinClient.GraphSON2MimeType);
+                //var resultFromSubmit = gremlinClient.SubmitAsync<dynamic>("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)").GetAwaiter().GetResult();
+                
+                 InsertNode("1", 1).GetAwaiter().GetResult();
+                
+                gremlinClient.Dispose();
             }
             else
             {
                 Console.WriteLine("Check the parameters");
             }
-
-            // IConfiguration config = new ConfigurationBuilder()
-            //     .AddEnvironmentVariables()
-            //     .Build();
-
-            // Console.WriteLine(config["cosmosdb_connectionstring"]);
-
-            // var gremlinServer = new GremlinServer(hostname, port, enableSsl: true,
-            //                                         username: "/dbs/" + database + "/colls/" + collection,
-            //                                         password: authKey);
-
-            // using (var gremlinClient = new GremlinClient(gremlinServer, new GraphSON2Reader(), new GraphSON2Writer(), GremlinClient.GraphSON2MimeType))
-            // {
-            //     foreach (var query in gremlinQueries)
-            //     {
-            //         Console.WriteLine(String.Format("Running this query: {0}: {1}", query.Key, query.Value));
-
-            //         // Create async task to execute the Gremlin query.
-            //         var task = gremlinClient.SubmitAsync<dynamic>(query.Value);
-            //         task.Wait();
-
-            //         foreach (var result in task.Result)
-            //         {
-            //             // The vertex results are formed as Dictionaries with a nested dictionary for their properties
-            //             string output = JsonConvert.SerializeObject(result);
-            //             Console.WriteLine(String.Format("\tResult:\n\t{0}", output));
-            //         }
-            //         Console.WriteLine();
-            //     }
-            // }
-
-
-            //InsertNode("1", 1).GetAwaiter().GetResult();
+           
             Console.WriteLine("Finished");
-
-
-
         }
 
         private static bool DoWeHaveAllParameters()
         {
             // ApiKind needs to be Gremlin
-            if(apiKind.ToLower() != "gremlin") return false;
+            if (apiKind.ToLower() != "gremlin") return false;
 
-            if(accountEndpoint != string.Empty && accountKey != string.Empty && database != string.Empty && collection != string.Empty)
+            if (accountEndpoint != string.Empty && accountKey != string.Empty && database != string.Empty && collection != string.Empty)
             {
                 return true;
             }
@@ -134,6 +124,9 @@ namespace cosmosdb_graph_test
                 {
                     case "accountendpoint":
                         accountEndpoint = GetValueFromPart(part);
+                        // we need to strip out some part of it
+                        accountEndpoint = Regex.Replace(accountEndpoint, "https://", "");
+                        accountEndpoint = Regex.Replace(accountEndpoint, @":443(\/)?", "");
                         break;
                     case "accountkey":
                         accountKey = GetValueFromPart(part);
@@ -163,9 +156,8 @@ namespace cosmosdb_graph_test
             return part.Split('=', 2)[0];
         }
 
-        static async Task InsertNode(string parentId, int level)
+        static async Task InsertNode(string id, int level)
         {
-
             int numberOfNodesToCreate = 0;
 
             if (level == 6) return;
@@ -194,19 +186,33 @@ namespace cosmosdb_graph_test
 
             string padding = new StringBuilder().Append('-', level).ToString();
 
-            Console.WriteLine($"{padding} {parentId}");
+            Console.WriteLine($"{padding} {id}");
 
-            //InsertNodeInCosmos();
+            InsertNodeInCosmos(id);
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < numberOfNodesToCreate; i++)
             {
-                await InsertNode(parentId + "-" + i.ToString(), level + 1);
+                await InsertNode(id + "-" + i.ToString(), level + 1);
             }
         }
 
-        static void InsertNodeInCosmos()
+        private static string CreateGremlinStatementToCreateAVertex(string id, int numberOfProperties = 20)
         {
-            //Task.Delay(500).Wait();
+            const string template = "g.addV('asset').property('id', '{0}')";
+            const string propertyTemplate = ".property('{0}', '{1}')";
+            StringBuilder sb = new StringBuilder(string.Format(template, id));
+
+            for (int i = 0; i < numberOfProperties; i++)
+            {
+                sb.Append(string.Format(propertyTemplate, $"prop{i}", $"value{i}"));    
+            }
+
+            return sb.ToString();
+        }
+
+        private static void InsertNodeInCosmos(string id)
+        {
+            var resultFromSubmit = gremlinClient.SubmitAsync<dynamic>(CreateGremlinStatementToCreateAVertex(id)).GetAwaiter().GetResult();
         }
 
     }
