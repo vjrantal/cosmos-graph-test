a4d1e9b86840dee6adb60da26354127c37bbe32b
Dag König <dagk@microsoft.com>
2018-08-24 14:59:52 +0200

    added tests

---
+++ test/cosmosdb-graph-test-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cosmosdb-graph-test-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cosmosdb-graph-test-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b175494-a14d-4515-bfe0-1725c878faca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/cosmosdb-graph-test-tests/UtilsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using cosmosdb_graph_test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cosmosdb_graph_test_tests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+        [TestMethod]
+        public void CreatePartitionKey_With_OneLevel()
+        {
+            var parameter = "1";
+            var answer = "1";
+
+            var result = Utils.CreatePartitionKey(parameter);
+
+            Assert.AreEqual(result, answer);
+        }
+
+        [TestMethod]
+        public void CreatePartitionKey_With_ThreeLevels_ShouldReturnTwoLevels()
+        {
+            var parameter = "1-2-3";
+            var answer = "1-2";
+
+            var result = Utils.CreatePartitionKey(parameter);
+
+            Assert.AreEqual(result, answer);
+        }
+    }
+}
